866d7e56232dca419f120b2c75e98c287c894639
cepeabarca <cepeabarca@gmail.com>
2021-10-20 23:58:15 -0500

    modificacion de la url por defecto

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace cursoASP
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Escuela}/{action=Index}/{id?}");
             });
         }
     }
