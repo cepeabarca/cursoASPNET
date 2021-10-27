794f125706bb02f6d3df283bec64d9c602a16d6a
cepeabarca <cepeabarca@gmail.com>
2021-10-27 00:22:23 -0500

    se agregan tablas de alumnos y asignaturas

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using cursoASP.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cursoASP.Controllers
+{
+    public class AlumnoController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View(new Alumno{Nombre="Pepe Perez",
+                            UniqueId = Guid.NewGuid().ToString()});
+        }
+
+        public IActionResult MultiAlumno()
+        {
+
+            var listaAlumno = GenerarAlumnosAlAzar();
+
+            //bolsa dinamica de variables o datos dinamicos
+            //ViewBag.Cosadinamica = "La Monja";
+            //ViewBag.Fecha = DateTime.Now;
+            //si no se especifica la vista, este regresa Index,
+            //el parametro es un modelo de datos que se usa en la vista
+            return View("MultiAlumno",listaAlumno);
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
+        }
+    }
+}
--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -3,24 +3,51 @@ using Microsoft.AspNetCore.Mvc;
 using cursoASP.Models;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace cursoASP.Controllers
 {
     public class AsignaturaController : Controller
     {
+
         public IActionResult Index()
         {
-            var asignatura = new Asignatura(){
-                UniqueId =Guid.NewGuid().ToString(),
+            return View(new Asignatura{Nombre="Programacion",
+                            UniqueId = Guid.NewGuid().ToString()});
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var asignatura = new Asignatura()
+            {
+                UniqueId = Guid.NewGuid().ToString(),
                 Nombre = "Programacion"
             };
 
+            var listaAsignaturas = new List<Asignatura>(){
+                        new Asignatura{Nombre="Matemáticas",
+                            UniqueId = Guid.NewGuid().ToString()
+                        } ,
+                        new Asignatura{Nombre="Educación Física",
+                            UniqueId = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura{Nombre="Castellano",
+                            UniqueId = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura{Nombre="Ciencias Naturales",
+                            UniqueId = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura{Nombre="Programacion",
+                            UniqueId = Guid.NewGuid().ToString()
+                        }
+            };
+
             //bolsa dinamica de variables o datos dinamicos
-            ViewBag.Cosadinamica = "La Monja";
-            ViewBag.Fecha = DateTime.Now;
+            //ViewBag.Cosadinamica = "La Monja";
+            //ViewBag.Fecha = DateTime.Now;
             //si no se especifica la vista, este regresa Index,
             //el parametro es un modelo de datos que se usa en la vista
-            return View(asignatura);
+            return View("MultiAsignatura",listaAsignaturas);
         }
     }
 }
